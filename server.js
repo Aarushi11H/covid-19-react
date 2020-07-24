12883d8148d2704a5cae14674d0d409c507004fc
Shashwat-Sahu <shashwatsahu25@gmail.com>
2020-07-24 22:39:35 +0530

    make it better

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ if(process.env.NODE_ENV==="production")
     app.use(express.static('client/covid-19/build'))
     const path = require('path')
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','covid-19','build','index.html'))
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
 }
 
